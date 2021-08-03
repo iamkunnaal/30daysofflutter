12e2084987ea58b2b28f26c47772596baffa5732
Kunal Goswami <kunalgoswami705@gmail.com>
2021-08-03 22:32:49 +0530

    Day2 complete

---
+++ first/lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+  final int days = 30;
+  final String name ="Kunal";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+        body: Center(
+          child: Container(
+            child: Text("Welcome to 30 $days of flutter by $name"),
+          ),
+        ),
+        drawer: Drawer(),
+      );
+  }
+}
